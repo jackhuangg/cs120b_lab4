fa5c4ba62026c5d219346e38aabd82e0f2625264
Jack Huang <jhuan314@wch136-24.cs.ucr.edu>
2021-04-18 15:03:41 -0700

    part 3 test

--- test/tests.py
+++ test/tests.py
@@ -18,17 +18,14 @@ tests = [ {'description': 'PINA: 0x01, 0x00, 0x02 => PORTB: 0x0F',
     'steps': [{'inputs': [('PINA', 0x04)], 'iterations': 2},
         {'inputs': [('PINA', 0x00)], 'iterations': 2},
         {'inputs': [('PINA', 0x02)], 'iterations': 2},
-        {'inputs': [('PINA', 0x00)], 'iterations': 2}
-],
+        {'inputs': [('PINA', 0x00)], 'iterations': 2}],
     'expected': [('PORTB',0x01)],
     },
-{'description': 'lock after unlocked',
+    {'description': 'bad input',
     'steps': [{'inputs': [('PINA', 0x04)], 'iterations': 2},
         {'inputs': [('PINA', 0x00)], 'iterations': 2},
-        {'inputs': [('PINA', 0x02)], 'iterations': 2},
-        {'inputs': [('PINA', 0x00)], 'iterations': 2}],
-    'expected': [('PORTB',0x00)],
+        {'inputs': [('PINA', 0xFF)], 'iterations': 2}],
+    'expected': [('PORTB',0x01)],
     },
     ]
 watch = ['SM1_STATE']
-
