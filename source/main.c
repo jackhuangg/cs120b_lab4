ba16cd85aa5b095a6b9367178ed4d9179af2a25e
Jack Huang <jhuan314@wch136-24.cs.ucr.edu>
2021-04-17 23:38:06 -0700

    awks

--- source/main.c
+++ source/main.c
@@ -13,135 +13,90 @@
 #endif
 
 volatile int TimerFlag = 0;
-
-enum SM1_STATES { SM1_SMStart, SM1_s0, SM1_s1, SM1_s2, SM1_s3, SM1_s5, SM1_s4, SM1_s6, SM1_s7} SM1_STATE;
-void Tick_StateMachine1() { 
+enum SM1_STATES { SM1_SMStart, SM1_s0, SM1_s1, SM1_s2, SM1_s3} SM1_STATE;
+void Tick_LoHi() { 
    switch(SM1_STATE) { 
       case SM1_SMStart:
          if (1) {
-            SM1_STATE = SM1_s5;
+            SM1_STATE = SM1_s0;
          }
          break;
       case SM1_s0:
-         if (PINA&&0x01) {
+         if (PINA==4) {
             SM1_STATE = SM1_s1;
-         }
-         else if ((PINA&&0x02)) {
-            SM1_STATE = SM1_s3;
-         }
-         else if ((PINA&&0x01) && (PINA&&0x02)) {
-            SM1_STATE = SM1_s4;
          }
          else {
             SM1_STATE = SM1_s0;
          }
          break;
       case SM1_s1:
-         if (1) {
+         if (PINA==4) {
+            SM1_STATE = SM1_s1;
+         }
+         else if (PINA==0) {
             SM1_STATE = SM1_s2;
+         }
+         else if (PINA&&0x04!=4) {
+            SM1_STATE = SM1_s0;
+         }
+         else {
+            SM1_STATE = SM1_s1;
          }
          break;
       case SM1_s2:
-         if ((PINA&&0x01) && (PINA&&0x02)) {
-            SM1_STATE = SM1_s4;
+         if (PINA==1) {
+            SM1_STATE = SM1_s3;
          }
-         else if (PINA&&0x01) {
-            SM1_STATE = SM1_s2;
+         else if (PINA||0x85) {
+            SM1_STATE = SM1_s0;
          }
-         else if (!(PINA&&0x01)) {
-            SM1_STATE = SM1_s7;
-         }
-         else {
+	 else {
             SM1_STATE = SM1_s2;
          }
          break;
       case SM1_s3:
-         if (1) {
-            SM1_STATE = SM1_s6;
+         if (PINA==2) {
+            SM1_STATE = SM1_s3;
          }
-         break;
-      case SM1_s5:
-         if (1) {
-            SM1_STATE = SM1_s0;
-         }
-         break;
-      case SM1_s4:
-         if (!((PINA&&0x01) && (PINA&&0x02))) {
-            SM1_STATE = SM1_s0;
-         }
-         else if ((PINA&&0x01) && (PINA&&0x02)) {
-            SM1_STATE = SM1_s4;
-         }
-         else {
-            SM1_STATE = SM1_s4;
-         }
-         break;
-      case SM1_s6:
-         if (!(PINA&&0x02)) {
-            SM1_STATE = SM1_s7;
-         }
-         else if ((PINA&&0x02)) {
-            SM1_STATE = SM1_s6;
-         }
-         else if ((PINA&&0x01) && (PINA&&0x02)) {
-            SM1_STATE = SM1_s4;
-         }
-         else {
-            SM1_STATE = SM1_s6;
-         }
-         break;
-      case SM1_s7:
-         if ((PINA&&0x02) || (PINA&&0x01)) {
+         else if (PINA==128) {
             SM1_STATE = SM1_s0;
          }
          else {
-            SM1_STATE = SM1_s7;
+            SM1_STATE = SM1_s3;
          }
          break;
       default:
-         SM1_STATE = SM1_s5;
+         SM1_STATE = SM1_s0;
          break;
    }
    switch(SM1_STATE) { 
-      case SM1_SMStart:     
+      case SM1_SMStart:        
          break;
       case SM1_s0:
+         PORTB=0;
          break;
-      case SM1_s1:
-         if(PORTC<9){
-	     PORTC=PORTC+1;
- 	 }
+      case SM1_s1:    
          break;
-      case SM1_s2:        
+      case SM1_s2:      
          break;
       case SM1_s3:
-         if(PORTC>0){
-	     PORTC=PORTC-1;
-	 }
-         break;
-      case SM1_s5:
-         PORTC=7;
-         break;
-      case SM1_s4:
-         PORTC=0;
-         break;
-      case SM1_s6:
-         break;
-      case SM1_s7:
+         PORTB=1;
          break;
    }
 }
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-    DDRA = 0x01;
-    DDRC = 0xFF;
+    DDRA = 0x00;
+    DDRB = 0xFF;
     PORTA = 0xFF;
-    PORTC = 0x01;
+    PORTB = 0x00;
     SM1_STATE = SM1_SMStart;
+	
     /* Insert your solution below */
    while(1){
-	Tick_StateMachine1();
+	Tick_LoHi();
    }
     return 1;
 }
+
