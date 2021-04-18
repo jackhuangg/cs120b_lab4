aac1b703693890917214ec3425f0c9ebfbe75017
Jack Huang <jhuan314@ucr.edu>
2021-04-18 15:34:15 -0700

    Update jhuan314_lab4_part2.c

--- turnin/jhuan314_lab4_part2.c
+++ turnin/jhuan314_lab4_part2.c
@@ -14,131 +14,125 @@
 
 volatile int TimerFlag = 0;
 
-enum SM1_STATES { SM1_SMStart, SM1_s0, SM1_s1, SM1_s2, SM1_s3, SM1_s5, SM1_s4, SM1_s6, SM1_s7} SM1_STATE;
+enum SM1_STATES { SM1_SMStart, check, plus, waitplus, minus, init, reset, waitminus, waitreset} SM1_STATE;
 void Tick_StateMachine1() { 
    switch(SM1_STATE) { 
       case SM1_SMStart:
-         if (1) {
-            SM1_STATE = SM1_s5;
-         }
+         SM1_STATE = init;
          break;
-      case SM1_s0:
+      case check:
          if (PINA&&0x01) {
-            SM1_STATE = SM1_s1;
+            SM1_STATE = plus;
          }
-         else if ((PINA&&0x02)) {
-            SM1_STATE = SM1_s3;
+         else if (PINA&&0x02) {
+            SM1_STATE = minus;
          }
-         else if ((PINA&&0x01) && (PINA&&0x02)) {
-            SM1_STATE = SM1_s4;
+         else if ((PINA&0x03) == 0x03) {
+            SM1_STATE = reset;
          }
          else {
-            SM1_STATE = SM1_s0;
+            SM1_STATE = check;
          }
          break;
-      case SM1_s1:
-         if (1) {
-            SM1_STATE = SM1_s2;
-         }
+      case plus:
+         SM1_STATE = waitplus;
          break;
-      case SM1_s2:
-         if ((PINA&&0x01) && (PINA&&0x02)) {
-            SM1_STATE = SM1_s4;
+      case waitplus:
+         if ((PINA&0x03) == 0x03) {
+            SM1_STATE = reset;
          }
          else if (PINA&&0x01) {
-            SM1_STATE = SM1_s2;
+            SM1_STATE = waitplus;
          }
          else if (!(PINA&&0x01)) {
-            SM1_STATE = SM1_s7;
-         }
-         else {
-            SM1_STATE = SM1_s2;
+            SM1_STATE = check;
          }
          break;
-      case SM1_s3:
+      case minus:
          if (1) {
-            SM1_STATE = SM1_s6;
+            SM1_STATE = waitminus;
          }
          break;
-      case SM1_s5:
+      case init:
          if (1) {
-            SM1_STATE = SM1_s0;
+            SM1_STATE = check;
          }
          break;
-      case SM1_s4:
+      case reset:
          if (!((PINA&&0x01) && (PINA&&0x02))) {
-            SM1_STATE = SM1_s0;
+            SM1_STATE = check;
          }
          else if ((PINA&&0x01) && (PINA&&0x02)) {
-            SM1_STATE = SM1_s4;
+            SM1_STATE = reset;
          }
          else {
-            SM1_STATE = SM1_s4;
+            SM1_STATE = reset;
          }
          break;
-      case SM1_s6:
-         if (!(PINA&&0x02)) {
-            SM1_STATE = SM1_s7;
+      case waitminus:
+         if ((PINA&0x02)==0x02) {
+            SM1_STATE = waitminus;
          }
-         else if ((PINA&&0x02)) {
-            SM1_STATE = SM1_s6;
-         }
-         else if ((PINA&&0x01) && (PINA&&0x02)) {
-            SM1_STATE = SM1_s4;
+	 else if ((PINA&0x03) == 0x03) {
+            SM1_STATE = reset;
          }
          else {
-            SM1_STATE = SM1_s6;
+            SM1_STATE = check;
          }
          break;
-      case SM1_s7:
-         if ((PINA&&0x02) || (PINA&&0x01)) {
-            SM1_STATE = SM1_s0;
+      case waitreset:
+         if ((PINA&0x03) == 0x03){
+            SM1_STATE = waitreset;
          }
          else {
-            SM1_STATE = SM1_s7;
+            SM1_STATE = check;
          }
          break;
       default:
-         SM1_STATE = SM1_s5;
+         SM1_STATE = SM1_SMStart;
          break;
    }
    switch(SM1_STATE) { 
-      case SM1_SMStart:     
+      case SM1_SMStart: 
+         PORTC=7;    
          break;
-      case SM1_s0:
+      case check:
          break;
-      case SM1_s1:
+      case plus:
          if(PORTC<9){
 	     PORTC=PORTC+1;
  	 }
          break;
-      case SM1_s2:        
+      case waitplus:        
          break;
-      case SM1_s3:
+      case minus:
          if(PORTC>0){
 	     PORTC=PORTC-1;
 	 }
          break;
-      case SM1_s5:
+      case init:
          PORTC=7;
          break;
-      case SM1_s4:
+      case reset:
          PORTC=0;
          break;
-      case SM1_s6:
+      case waitminus:
          break;
-      case SM1_s7:
+      case waitreset:
+         break;
+      default:
+	 PORTC=7;
          break;
    }
 }
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-    DDRA = 0x01;
+    DDRA = 0x00;
     DDRC = 0xFF;
     PORTA = 0xFF;
-    PORTC = 0x01;
-    SM1_STATE = SM1_SMStart;
+    PORTC = 0x00;
+	
     /* Insert your solution below */
    while(1){
 	Tick_StateMachine1();
